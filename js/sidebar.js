791092e2118c940ccbaedbf63f1982d3c0b1332f
salarshirkhani <salarshirkhany16@gmail.com>
2023-07-16 18:56:49 +0330

    add js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,17 @@
-var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyOSIsImp0aSI6ImNmZjkyYTBlLWI5ZWEtNDUwMC04M2Q0LWNiZjY2NGE5ZjEzYSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiMDkxMjc5NzYxOTAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsicHVibGljIiwiYWRtaW5UaWNrZXQiXSwiZXhwIjoxNjg5NDQzNTQ0LCJpc3MiOiJNS0giLCJhdWQiOiJNS0gifQ.OIUUN0DtbTz5pfMiHOqOjuM-2_VBsuWF6FAWdopIi8I';
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyOSIsImp0aSI6IjE2MGMyMWExLWQyODQtNGQyZi05MWQ0LTYyM2VhODQxYjAyOSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiMDkxMjc5NzYxOTAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsicHVibGljIiwiYWRtaW5UaWNrZXQiXSwiZXhwIjoxNjg5NTI1NjExLCJpc3MiOiJNS0giLCJhdWQiOiJNS0gifQ.RGa3s8LGiFp6BmlkwJ_dCqFPDHPVTNalX74UT9paXC0';
 kh_main.service.get("userTicket/Tickets", function (response) {
 
     kh_main.Loding.hide();
     if (response.messageType == 1) {
-        alert('سرویس با موفقیت اجرا شد');
-        debugger
-        console.log(response.objectResult);
+        var data = response.objectResult;
+        for (let index = 0; index < data.length; index++) {
+            const item = data[index];
+
+            var row = $('.contacts .temprow .users').clone();
+            $('h3' , row).html(item.subject);
+
+            $('.contacts').append(row);
+        }
     }
     else {
         console.log(response);
@@ -15,3 +21,54 @@ kh_main.service.get("userTicket/Tickets", function (response) {
 },token);
 
 
+function  GetChats(status){
+    var link = 'userTicket/Tickets?status='+status ;
+    kh_main.service.get(link, function (response) {
+
+        kh_main.Loding.hide();
+        if (response.messageType == 1) {
+            var data = response.objectResult;
+            for (let index = 0; index < data.length; index++) {
+                const item = data[index];
+    
+                var row = $('.contacts .temprow .users').clone();
+                $('h3' , row).html(item.subject);
+                $('#content' , row).html(item.description);
+                var day = new Date(item.createTime);
+                $('.date' , row).html('14t');
+                $('.contacts').append(row);
+            }
+        }
+        else {
+            console.log(response);
+            alert(response.message);
+        }
+    
+    },token);
+}
+
+function  GetMessage(status){
+    var link = 'userTicket/Tickets?status='+status ;
+    kh_main.service.get(link, function (response) {
+
+        kh_main.Loding.hide();
+        if (response.messageType == 1) {
+            var data = response.objectResult;
+            for (let index = 0; index < data.length; index++) {
+                const item = data[index];
+    
+                var row = $('.contacts .temprow .users').clone();
+                $('h3' , row).html(item.subject);
+                $('#content' , row).html(item.description);
+                var day = new Date(item.createTime);
+                $('.date' , row).html('14t');
+                $('.contacts').append(row);
+            }
+        }
+        else {
+            console.log(response);
+            alert(response.message);
+        }
+    
+    },token);
+}
