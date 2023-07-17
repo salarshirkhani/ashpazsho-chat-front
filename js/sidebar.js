0f98cebcf182cda2b2cf0564787f40af466b3d37
salarshirkhani <salarshirkhany16@gmail.com>
2023-07-17 21:01:34 +0330

    add functions

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,4 @@
-var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyOSIsImp0aSI6IjlmOGY2ZjZjLWMzOTYtNDI5YS1hZjY1LTM4MjFmNDhmN2VhNCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiMDkxMjc5NzYxOTAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsicHVibGljIiwiYWRtaW5UaWNrZXQiXSwiZXhwIjoxNjg5NjEzNDU1LCJpc3MiOiJNS0giLCJhdWQiOiJNS0gifQ.hMD674TAHdsmJCEOUMwWjDKgEVP2Ojvmp-Wq6QdsU5w';
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyOSIsImp0aSI6IjRhZmUxMjljLTFhNzMtNDFhNi04ZTNmLWMyZDdmOGEzNzA0NyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiMDkxMjc5NzYxOTAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsicHVibGljIiwiYWRtaW5UaWNrZXQiXSwiZXhwIjoxNjg5NjIyMTA0LCJpc3MiOiJNS0giLCJhdWQiOiJNS0gifQ.ORsG3gSNLF82qOaeIa_SRo9LyqwWvSGG4YpudrintcU';
 kh_main.service.get("userTicket/Tickets", function (response) {
 
     kh_main.Loding.hide();
@@ -35,7 +35,6 @@ function  GetChats(status){
                 $(row).attr('data-id',item.id)
                 $('h3' , row).html(item.subject);
                 $('#content' , row).html(item.description);
-                debugger
                 $('.date' , row).html('14t');
                 $('.contacts').append(row);
             }
@@ -50,15 +49,15 @@ function  GetChats(status){
 
 function  GetMessage(tag){
     var id = $(tag).attr('data-id');
-    var link = '/userTicket/chats?chatinfo='+id ;
-    kh_main.service.get(link, function (response) {
-
+    var link = 'userTicket/Chats?chatinfo='+id ;
+        kh_main.service.get(link, function (response) {
         kh_main.Loding.hide();
         if (response.messageType == 1) {
             var data = response.objectResult;
             for (let index = 0; index < data.length; index++) {
                 const item = data[index];
-                if(item.senderid = kh_main.cookie.getvalue('userid')){
+                //if(item.senderid = kh_main.cookie.getvalue('userid')){
+                if(item.senderid == '29'){
                     var row = $('.chat .temprow .message-right').clone();
                     if(item.answer_id == NULL){
                        $('.reply',row).remove();
@@ -71,7 +70,8 @@ function  GetMessage(tag){
                     $('.main-content' , row).attr('data-id',item.id);
                 }
 
-                if(item.senderid != kh_main.cookie.getvalue('userid')){
+                //if(item.senderid != kh_main.cookie.getvalue('userid')){
+                if(item.senderid == '29'){
                     var row = $('.chat .temprow .message-left').clone();
                     if(item.answer_id == NULL){
                        $('.reply',row).remove();
