6bc4bc9fdfafd1f21f8c92dfedfd0ed8102f47fe
salarshirkhani <salarshirkhany16@gmail.com>
2023-07-18 20:06:13 +0330

    add chats

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-// diagram dashborad ba-ham
-var xValues = ["شنبه", "یکشنبه", "دوشنبه", "سه شنبه", "چهار شنبه", "پنج شنبه", "جمعه"];
-var yValues = [100, 49, 44, 25, 0];
-var barColors = ["red", "green","blue","orange","brown"];
-
-new Chart("myChart", {
-  type: "bar",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    title: {
-      display: true,
-    }
-  }
-});
 ///menu-bar-mobile js
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,25 +1,35 @@
-var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyOSIsImp0aSI6IjRhZmUxMjljLTFhNzMtNDFhNi04ZTNmLWMyZDdmOGEzNzA0NyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiMDkxMjc5NzYxOTAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsicHVibGljIiwiYWRtaW5UaWNrZXQiXSwiZXhwIjoxNjg5NjIyMTA0LCJpc3MiOiJNS0giLCJhdWQiOiJNS0gifQ.ORsG3gSNLF82qOaeIa_SRo9LyqwWvSGG4YpudrintcU';
-kh_main.service.get("userTicket/Tickets", function (response) {
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyOSIsImp0aSI6ImZlZjBkMzVlLWU4YWEtNDU3Mi05MmRlLWYzYmM3MTFiMjAxZSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiMDkxMjc5NzYxOTAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsicHVibGljIiwiYWRtaW5UaWNrZXQiXSwiZXhwIjoxNjg5NzAwNTU4LCJpc3MiOiJNS0giLCJhdWQiOiJNS0gifQ.BZc5KceRMcfHnZLXES4f74ntOhTnF3Mju_Cs2HR9YVo';
 
-    kh_main.Loding.hide();
-    if (response.messageType == 1) {
-        var data = response.objectResult;
-        for (let index = 0; index < data.length; index++) {
-            const item = data[index];
-
-            var row = $('.contacts .temprow .users').clone();
-            $('h3' , row).html(item.subject);
-
-            $('.contacts').append(row);
+function  Index(){
+    var link = 'userTicket/Tickets?status=all';
+    $('.send-message').css('display','none');
+    var row = $('.chat .temprow .nothing').clone();
+    $(row).removeClass('temp');
+    $('h3' , row).html('هنوز هیچ موردی رو انتخاب نکردی');
+    $('p' , row).html('چت مورد نظرت رو انتخاب کن تا پیاماشو ببینی');
+    $('.chat').append(row);
+    kh_main.service.get(link, function (response) {
+        kh_main.Loding.hide();
+        if (response.messageType == 1) {
+            var data = response.objectResult;
+            for (let index = 0; index < data.length; index++) {
+                const item = data[index];
+    
+                var row = $('.contacts .temprow .users').clone();
+                $(row).attr('data-id',item.id)
+                $('h3' , row).html(item.subject);
+                $('#content' , row).html(item.description);
+                $('.date' , row).html('14t');
+                $('.contacts').append(row);
+            }
         }
-    }
-    else {
-        console.log(response);
-        alert(response.message);
-    }
-
-},token);
-
+        else {
+            console.log(response);
+            alert(response.message);
+        }
+    
+    },token);
+}
 
 function  GetChats(status){
     var link = 'userTicket/Tickets?status='+status ;
@@ -49,31 +59,41 @@ function  GetChats(status){
 
 function  GetMessage(tag){
     var id = $(tag).attr('data-id');
+    $('.chat .nothing:not(.temp)').remove();
+    $('.send-message').css('display','inline-flex');
     var link = 'userTicket/Chats?chatinfo='+id ;
         kh_main.service.get(link, function (response) {
         kh_main.Loding.hide();
         if (response.messageType == 1) {
             var data = response.objectResult;
+            if(data.length== 0){
+                var row = $('.chat .temprow .nothing').clone();
+                $(row).removeClass('temp');
+                $('h3' , row).html('هنوز پیامی فرستاده نشده');
+                $('p' , row).html('هرچی دل تنگت میخواد بنویس :)');
+                $('.chat').append(row);
+            }
             for (let index = 0; index < data.length; index++) {
                 const item = data[index];
                 //if(item.senderid = kh_main.cookie.getvalue('userid')){
-                if(item.senderid == '29'){
-                    var row = $('.chat .temprow .message-right').clone();
-                    if(item.answer_id == NULL){
-                       $('.reply',row).remove();
+                if(item.senderId == '29'){
+                    var row2 = $('.chat .temprow .message-right').clone();
+                    if(item.answerId == undefined){
+                       $('.contain .reply',row2).remove();
                     }
                     else{
-                        $('.reply span' , row).html(item.answercontent);
-                        $('.reply span' , row).attr('data-answerid',item.answerId);
+                        $('.reply span' , row2).html(item.answercontent);
+                        $('.reply span' , row2).attr('data-answerid',item.answerId);
                     }
-                    $('.main-content' , row).html(item.content);
-                    $('.main-content' , row).attr('data-id',item.id);
+                    $('.main-content' , row2).html(item.content);
+                    $('.main-content' , row2).attr('data-id',item.id);
+                    $('.chat').append(row2);
                 }
 
                 //if(item.senderid != kh_main.cookie.getvalue('userid')){
-                if(item.senderid == '29'){
+                if(item.senderId != '29'){
                     var row = $('.chat .temprow .message-left').clone();
-                    if(item.answer_id == NULL){
+                    if(item.answerId == NULL){
                        $('.reply',row).remove();
                     }
                     else{
